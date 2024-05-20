f16b4210d75475653246b5e613b54fdd842206c5
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-05-20 15:53:06 +0400

    show-recent-currency-rates completed

--- Azn-task/Program.cs
+++ Azn-task/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string[] Codes = { "USD", "RUB", "TRY" };
+            double[] Rates = { 0.59, 53.46, 18.94 };
+            Console.WriteLine("if you want to show recent currency rates click 1:");
+            Console.WriteLine("if you want to find currency rate by spesific code click 2:");
+            Console.WriteLine("if you want to calculate amount by currency click 3 and enter the amount with AZN:");
+            int click_buttonint=int.Parse(Console.ReadLine());
+            if(click_buttonint==1 )
+            {
+                for(int i=0;i<3;i++) 
+                {
+                    Console.WriteLine($"1 AZN={Rates[i]} {Codes[i]}");
+                }
+            }
+            
+
         }
     }
 }
